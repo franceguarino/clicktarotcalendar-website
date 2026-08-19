5b1aecc22a2fbcfb463fdf78cb099fee6eb58554
franceguarino <francescoguarino@scuolatdm.com>
2026-08-19 17:57:47 +0200

    Add social icons to footer
    Instagram, Facebook and YouTube links with inline SVG icons, shown on
    every page across all locales.

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,9 @@
 export const CHECKOUT_URL = 'https://checkout.freemius.com/plugin/36819/plan/61064/';
 export const CONTACT_EMAIL = 'clicktarot@clicktarot.net';
 export const SITE_URL = 'https://clicktarotcalendar.com';
+
+export const SOCIAL_LINKS = {
+	instagram: 'https://www.instagram.com/franceguarino/',
+	facebook: 'https://www.facebook.com/scuolatdm',
+	youtube: 'https://www.youtube.com/channel/UCHxmNaZUDZzL3GofQ6ETkPQ?sub_confirmation=1',
+} as const;
